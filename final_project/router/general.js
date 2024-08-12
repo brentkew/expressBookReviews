0042dac04b284aa1a1d606d33d9ef00f3d8778f0
Khalil Ahmad <brentkew@gmail.com>
2024-08-12 19:46:06 +0500

    Task 5 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -61,8 +61,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; // Retrieve ISBN from request parameters
+  let bookFound = null;
+  for (const key in books) {
+    if (books[key].isbn === isbn) {
+      bookFound = books[key];
+      break;
+    }
+  }
+
+  if (bookFound) {
+      return res.status(200).json(bookFound.reviews);
+  } else {
+      return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
